a4dcb6421ed8a4c9a484c5ed7e69f96c9777c881
王星辰 <88174774+Star1128@users.noreply.github.com>
2022-03-14 14:52:07 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/gradequery/MainActivity.java
+++ app/src/main/java/com/example/gradequery/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
         List<User> students = getUserFromSheet();
 
